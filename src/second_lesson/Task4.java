68b842323560dfbab191d6a84095bd92f9ea37b4
CurtisEllese <mama-karina@bk.ru>
2024-04-13 16:51:44 +0300

    Add strucrure of Task 4

---
+++ src/second_lesson/Task4.java
@@ -0,0 +1,38 @@
+// Запишите в файл следующие строки:
+//Анна=4
+//Елена=5
+//Марина=6
+//Владимир=?
+//Константин=?
+//Иван=4
+//Реализуйте метод, который считывает данные из файла и сохраняет в двумерный массив (либо HashMap, если
+//студенты с ним знакомы). В отдельном методе нужно будет пройти по структуре данных, если сохранено
+//значение ?, заменить его на соответствующее число.Если на каком-то месте встречается символ, отличный от
+//числа или ?, бросить подходящее исключение.Записать в тот же файл данные с замененными символами ?
+
+package second_lesson;
+
+import java.io.BufferedReader;
+import java.io.BufferedWriter;
+import java.io.FileReader;
+import java.io.FileWriter;
+
+public class Task4 {
+    public static void main(String[] args) {
+
+    }
+
+//    BufferedReader br = new BufferedReader(new FileReader(patch));
+//    String line = "";
+//while ((line = br.readLine()) != null) {
+//
+//    }
+//
+//
+//
+//    BufferedWriter bw = new BufferedWriter(new FileWriter(patch));
+//for (String[] strings : arrays) {
+//        bw.write(strings[0] + "=" + strings[1] + "\n");
+//    }
+//bw.close();
+}
